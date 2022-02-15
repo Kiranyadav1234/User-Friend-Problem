3db3d48fb455685b94dee5bf6a708bc290f31cbc
Kiranyadav1234 <Kiran_Yadav@mckinsey.com>
2022-02-15 08:27:32 +0530

    feat:add test code using spyOn and try-catch method

--- TEST/userFriendNamePrinter.test.js
+++ TEST/userFriendNamePrinter.test.js
@@ -9,7 +9,7 @@ describe('printFriendName function',()=>
      printFriendName('Barton Mckee');
       expect(spy).toHaveBeenCalledTimes(1);
     });
-    it('should check the output printed by console',()=>{
+    it('should give  the friends name and id if given user exist',()=>{
       const spy=jest.spyOn(console,'log');
       printFriendName('Barton Mckee');
       //in order to check the value printed by console.
@@ -19,12 +19,30 @@ describe('printFriendName function',()=>
         { id: 2, name: 'Krystal Sheppard' }
       ]);
     });
+    it('should throw error if given user does not exist',()=>{
+      try{
+        const result=printFriendName('kiran');
+
+      }catch(err){
+
+            expect(err.message).toBe('User does not exist');
+      }
+             
+
+    });
     it('should throw error if a number is given as input',()=>{
       expect(()=>printFriendName(123)).toThrow('Invalid Input');
 
     });
     it('should throw error if an array is given as input',()=>{
-      expect(()=>printFriendName([1,2,3])).toThrow('Invalid Input');
+      try{
+        const result=printFriendName([1,2,3]);
+
+      }catch(err){
+  
+            expect(err.message).toBe('Invalid Input');
+      }
+             
 
     });
       
